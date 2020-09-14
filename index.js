f8daec714d10cfc9572802547f1bc35c55e23d7c
Maciej <maciejolsz@gmail.com>
2020-09-14 11:40:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ function makeMagic() {
   document.getElementById("magic-to-make").innerHTML = "<img src='https://media3.giphy.com/media/12NUbkX6p4xOO4/giphy.gif' />";
   document.getElementById("magic-to-make").innerHTML += "<br />✨💖ⓟⓗⓟ ⓢⓢⓘⓔ🦄✨ jeszcze bardziej";
 }
+
+stronaZaladowana() { document.getElementsByTagName("Iframe").remove(); }
