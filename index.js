9b4b1de59bbc6e3a3d6f93a2c4c8709a296213ca
Róża Szylar <roseaszy@gmail.com>
2020-09-14 11:12:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 function makeMagic() {
   document.getElementById("magic-to-make").innerHTML = "✨💖ⓟⓗⓟ ⓢⓢⓘⓔ🦄✨";
   document.getElementById("magic-to-make").innerHTML = "<img src='https://media3.giphy.com/media/12NUbkX6p4xOO4/giphy.gif' />";
+  document.getElementById("magic-to-make").innerHTML += "<br />✨💖ⓟⓗⓟ ⓢⓢⓘⓔ🦄✨ jeszcze bardziej";
 }
